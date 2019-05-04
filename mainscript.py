8294d344737045bffb1a734c279cc8646fdd587c
Sarthak Saini <46784458+geek-repo@users.noreply.github.com>
2019-05-04 13:15:39 +0530

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -1,5 +1,7 @@
 import base64
 from elasticsearch import Elasticsearch
+import re
+import datetime
 
 es=Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
@@ -11,18 +13,45 @@ def dehash(data):
     decoded=data = base64.b64decode(data)
     return (decoded.decode())
 
-def transfer(data):
+def transfer(data,date):
 
+    f=re.findall("[0-9].",date)
+
+    hour,minutes=timer(f[0])
 
     body={
 
-    'data':hashit(data)
-
+    'data':hashit(data),
+    'hour':hour,
+    'minutes':minutes,
     }
+    try:
+        resh=es.index(index='storage',doc_type='dbs',body=body)
+        send="https://79b986f8.ngrok.io/{}".format(resh['_id'])
+        return(send)
+    except:
+        return("Error in Database")
 
-    resh=es.index(index='storage',doc_type='dbs',body=body)
-    send="https://79b986f8.ngrok.io/{}".format(resh['_id'])
-    return(send)
+
+def timer(min):
+
+    now=datetime.datetime.now()
+    hour=now.hour
+    if min=="60":
+        hour=now.hour+1
+        minutes=now.minute
+
+    else:
+        minutes=now.minute+int(min)
+        if minutes>60:
+            hour=now.hour+1
+            minutes=minutes-60
+
+
+    return hour,minutes
+
+
+
 
 def showdata(data):
 
